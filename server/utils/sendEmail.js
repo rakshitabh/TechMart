9d06b3c1b82643c8f8d961ebddc970e42d407287
Rakshita Bhat <rakshitalbhat07@gmail.com>
2026-07-03 23:08:06 +0530

    Final

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -3,8 +3,8 @@ import nodemailer from 'nodemailer';
 const sendEmail = async (options) => {
   const transporter = nodemailer.createTransport({
     host: process.env.BREVO_SMTP_HOST,
-    port: Number(process.env.BREVO_SMTP_PORT),
-    secure: false, // Port 587
+    port: 465,
+    secure: true,
     auth: {
       user: process.env.BREVO_SMTP_USER,
       pass: process.env.BREVO_SMTP_PASS,
